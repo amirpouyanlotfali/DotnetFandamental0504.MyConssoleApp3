8e6c0f89ca3e7b9c721ad06cac7c86bff441bb6b
Amirpouyanlotfali <amirpouyan.lotfali@gmail.com>
2026-07-23 15:14:00 +0330

    Add project files.

---
+++ DotnetFandamental0504.MyConssoleApp3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotnetFandamental0504.MyConssoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
